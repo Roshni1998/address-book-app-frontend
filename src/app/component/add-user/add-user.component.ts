ba118dcd30bf9b39a68175b30a97658bc417868f
Roshni1998 <roshnimali847@gmail.com>
2021-09-05 01:54:46 +0530

    Added: Creating Person Address Form and add scss to it

--- src/app/component/add-user/add-user.component.ts
+++ src/app/component/add-user/add-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AddressBook } from 'src/app/model/address-book';
+import { HttpService } from 'src/app/service/http.service';
 
 @Component({
   selector: 'app-add-user',
@@ -7,7 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor() { }
+  public person: AddressBook = new AddressBook();
+  personFormGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private httpService: HttpService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute,) { 
+                this.personFormGroup = this.formBuilder.group({
+                  fullName: new FormControl(''),
+                  address: new FormControl(''),
+                  city: new FormControl(''),
+                  state: new FormControl(''),
+                  zipNo: new FormControl(''),
+                  phoneNumber: new FormControl('')
+                });
+              }
 
   ngOnInit(): void {
   }
